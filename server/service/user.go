cf6d7d2de6f7b074e5b91b305326a13db8681214
oxicuza <oxicusa@gmail.com>
2022-10-27 23:59:00 +0700

    add user service for handle create user

--- server/service/user.go
+++ server/service/user.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"final-project/server/params"
 	"final-project/server/repository"
+	"final-project/server/view"
+	"log"
+	"time"
 )
 
 type UserService struct {
@@ -12,4 +16,21 @@ func NewUserService(repository repository.UserRepository) *UserService {
 	return &UserService{
 		repository: repository,
 	}
+}
+
+func (u *UserService) CreateUser(request *params.User) *view.Response {
+	user, err := request.ParseToModel()
+	if err != nil {
+		log.Printf("Error when casting request to user model %v\n", err)
+		return view.ErrorRequest("CREATED_USER_FAIL", "BAD_REQUEST")
+	}
+
+	// TODO: change id value to ...
+	user.Id = "string random"
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	// TODO: change to dynamic
+	user.Role = "user"
+
+	return view.UserCreated("CREATED_USER_SUCCESS")
 }
