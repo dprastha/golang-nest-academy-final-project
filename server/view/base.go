46a8f13264925015efd22dd189925dfb5085ce79
dprastha <danielprastha4@gmail.com>
2022-10-27 21:15:47 +0700

    improve view response

--- server/view/base.go
+++ server/view/base.go
@@ -17,14 +17,15 @@ type Response struct {
 }
 
 var ErrMap = map[string]interface{}{
-	"BAD_REQUEST":          gin.H{"message": "invalid request payload"},
-	"FORBIDDEN_ACCESS":     gin.H{"message": "you dont have access for this resources"},
-	"UNAUTHORIZED":         gin.H{"message": "you need to login for access this resources"},
-	"NOT_FOUND":            gin.H{"message": "data not found in this resources"},
-	"UNPROCESSABLE_ENTITY": gin.H{"message": "stock prodcut not enough"},
+	"BAD_REQUEST":           gin.H{"message": "invalid request payload"},
+	"FORBIDDEN_ACCESS":      gin.H{"message": "you dont have access for this resources"},
+	"UNAUTHORIZED":          gin.H{"message": "you need to login for access this resources"},
+	"NOT_FOUND":             gin.H{"message": "data not found in this resources"},
+	"UNPROCESSABLE_ENTITY":  gin.H{"message": "stock prodcut not enough"},
+	"INTERNAL_SERVER_ERROR": gin.H{"message": "internal server error"},
 }
 
-func SuccessRequest(msg string) *Response {
+func SuccessResponse(msg string, statusCode int) *Response {
 	return &Response{
 		Status:      http.StatusOK,
 		Message:     msg,
@@ -32,9 +33,9 @@ func SuccessRequest(msg string) *Response {
 	}
 }
 
-func ErrorRequest(msg string, err string) *Response {
+func ErrorResponse(msg string, err string, statusCode int) *Response {
 	return &Response{
-		Status:      http.StatusBadRequest,
+		Status:      statusCode,
 		Message:     msg,
 		Error:       err,
 		AddInfo:     ErrMap[err],
