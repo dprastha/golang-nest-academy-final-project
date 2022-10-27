b4e65018ee91d53bf56da89ba254aa97d50b0d27
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-10-27 22:13:20 +0700

    repo product

---
+++ server/repository/product.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"errors"
+	"final-project/server/model"
+
+	"gorm.io/gorm"
+)
+
+type productRepo struct {
+	db *gorm.DB
+}
+
+func NewProductRepo(db *gorm.DB) productRepo {
+	return productRepo{
+		db: db,
+	}
+}
+
+func (p *productRepo) GetAllProducts() (*[]model.Product, error) {
+	var product []model.Product
+
+	err := p.db.Find(&product).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
+
+func (p *productRepo) GetProductById(productId string) (*model.Product, error) {
+	var product model.Product
+
+	err := p.db.Where("id = ?", productId).First(&product).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
+
+func (p *productRepo) CreateProduct(product *model.Product) error {
+	return p.db.Create(product).Error
+}
+
+func (p *productRepo) UpdateProduct(productId string, product *model.Product) error {
+
+	if p.db.Model(product).Where("id = ?", productId).Updates(product).RowsAffected == 0 {
+		return errors.New("QUERY_NOT_AFFECTED")
+	}
+
+	return nil
+}
+
+func (p *productRepo) DeleteProduct(productId string) error {
+	var product model.Product
+
+	if p.db.Where("id = ?", productId).Delete(&product).RowsAffected == 0 {
+		return errors.New("QUERY_NOT_AFFECTED")
+	}
+
+	return nil
+}
