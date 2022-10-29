7632fa52380c8fe3cc771785c58dc6675e2a3b72
Muhammad Inayatulloh <ainmtsn1999@gmail.com>
2022-10-29 11:38:51 +0700

    update-client-test-post

--- pkg/httpclient/client_test.go
+++ pkg/httpclient/client_test.go
@@ -15,7 +15,16 @@ type JSONBaseGet struct {
 type JSONGet struct {
 	Query   interface{}
 	Status  interface{}
-	Results interface{}
+	Results JSONResultsGet
+}
+
+type JSONResultsGet struct {
+	City_id     string
+	Province_id string
+	Province    string
+	Kabupaten   string
+	City_name   string
+	Postal_code string
 }
 
 type JSONBasePost struct {
@@ -32,12 +41,30 @@ type JSONPost struct {
 
 func TestGet(t *testing.T) {
 	client := NewHttpClient("https://api.rajaongkir.com/starter", "6d4d26125ea1c2991b801880cf3842f7")
-	resp, err := client.Get("/city?id=12")
+	resp, err := client.Get("city?id=39")
 	assert.Nil(t, err)
 
 	var datas JSONBaseGet
 
 	err = json.Unmarshal(resp, &datas)
 	assert.Nil(t, err)
+	fmt.Println(datas.Rajaongkir.Results)
+}
+
+func TestPost(t *testing.T) {
+	client := NewHttpClient("https://api.rajaongkir.com/starter", "6d4d26125ea1c2991b801880cf3842f7")
+	resp, err := client.Post("cost", map[string]interface{}{
+		"origin": "10",
+		"destination":  "15",
+		"weight":   1700,
+		"courier": "jne",
+	})
+	assert.Nil(t, err)
+
+	var datas JSONBasePost
+
+	err = json.Unmarshal(resp, &datas)
+	assert.Nil(t, err)
+
 	fmt.Println(datas.Rajaongkir)
 }
